9a08370e33f3196dde7f252e50f40f1cf916dcad
Mohamed-Ossalhe <ossalhemohamed2@gmail.com>
2023-03-03 11:43:36 +0100

    disable reserved times for every specific day

--- frontend/src/components/calendar/ReservationTime.jsx
+++ frontend/src/components/calendar/ReservationTime.jsx
@@ -20,17 +20,24 @@ const ReserveTime = ({times}) => {
         <div className="selectTime">
             <label htmlFor="times" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Appiontement Time:</label>
             <select id="times" size="6" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onClick={getTimeSelected}>
-                {filterTimes.length !== 0 ? filterTimes.map((time) => {
-                    return (
-                        <option key={time} value={time}>{time}</option>
-                    )
-                }): ""}
-                {/* <option value="08:00">08:00</option>
-                <option value="09:00">09:00</option>
-                <option value="10:00">10:00</option>
-                <option value="11:00">11:00</option>
-                <option value="14:00">14:00</option>
-                <option value="15:00">15:00</option> */}
+            {availabletimes.map((time) => {
+                // Check if the current time is reserved
+                const isReserved = reservedTimes.includes(time);
+
+                return (
+                    <option key={time} value={time} disabled={isReserved}>
+                        {time}{isReserved && " (Reserved)"}
+                    </option>
+                )
+            })}
+                {/* {filterTimes.length !== 0 ? 
+                    (
+                    filterTimes.map((time) => {
+                        return (
+                            <option key={time} value={time}>{time}</option>
+                        )
+                    })
+                ) : (<option disabled>No Times Available</option>)} */}
             </select>
         </div>
     )
